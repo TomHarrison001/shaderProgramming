0b2f2918ab6c734fe28c7881a61b27a16e4c1ba0
TomHarrison001 <p2712155@my365.dmu.ac.uk>
2023-10-31 08:41:15 +0000

    Refactor myScene

--- Application_Clean/include/myScene.h
+++ Application_Clean/include/myScene.h
@@ -2,60 +2,17 @@
 #include "Scene.h"
 #include "Shader.h"
 #include "Lights/DirectionalLight.h"
+#include "Cube/Cube.h"
 
 class MyScene : public Scene {
 public:
 	MyScene(GLFWwindow* window, InputHandler* H);
-	~MyScene() {};
+	~MyScene();
 	void update(float dt) override;
 private:
 	Shader* m_myShader;
 	DirectionalLight* m_directionalLight;
-	int jumpFrame = 51;
-	float gravity;
-	unsigned int VBO, VAO, EBO;
-	bool jumping;
-	glm::vec3 pos, rot, scale = glm::vec3(1.0, 1.0, 1.0);
-	glm::mat4 m_model, m_view, m_projection;
-	std::vector<float> vertexData = {
-		// back
-		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
-		 0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
-		 0.5f,  0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
-		-0.5f,  0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
-		// front
-		-0.5f, -0.5f,  0.5f, 0.0f, 0.0f, 1.0f,
-		 0.5f, -0.5f,  0.5f, 0.0f, 0.0f, 1.0f,
-		 0.5f,  0.5f,  0.5f, 0.0f, 0.0f, 1.0f,
-		-0.5f,  0.5f,  0.5f, 0.0f, 0.0f, 1.0f,
-		// left
-		-0.5f,  0.5f,  0.5f, -1.0f, 0.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
-		-0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
-		-0.5f, -0.5f,  0.5f, -1.0f, 0.0f, 0.0f,
-		// right
-		0.5f,  0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
-		0.5f,  0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-		0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-		0.5f, -0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
-		// bottom
-		-0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
-		 0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
-		 0.5f, -0.5f,  0.5f, 0.0f, -1.0f, 0.0f,
-		-0.5f, -0.5f,  0.5f, 0.0f, -1.0f, 0.0f,
-		// top
-		-0.5f,  0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-		 0.5f,  0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-		 0.5f,  0.5f,  0.5f, 0.0f, 1.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f, 0.0f, 1.0f, 0.0f };
-	std::vector<unsigned int> indices = {
-		0, 1, 2, 2, 3, 0,
-		4, 5, 6, 6, 7, 4,
-		8, 9, 10, 10, 11, 8,
-		12, 13, 14, 14, 15, 12,
-		16, 17, 18, 18, 19, 16,
-		20, 21, 22, 22, 23, 20 };
-	void makeVAO();
+	Cube* m_cube;
+
 	void render();
-	void cubeJump();
 };
--- Application_Clean/src/myScene.cpp
+++ Application_Clean/src/myScene.cpp
@@ -7,78 +7,34 @@ MyScene::MyScene(GLFWwindow* window, InputHandler* H) : Scene(window, H) {
 	m_myShader = new Shader("..\\Shaders\\vertShader.glsl", "..\\Shaders\\fragShader.glsl");
 	m_directionalLight = new DirectionalLight(glm::vec3(1.0, 1.0, 1.0), glm::vec3(-1.0, -1.0, -1.0));
 	m_directionalLight->setLightUniforms(m_myShader);
-	makeVAO();
+	
+	m_cube = new Cube(glm::vec3(0.5, 0.0, 0.0), 64, 16);
+	m_cube->setCubeMaterialValues(m_myShader);
 }
-void MyScene::makeVAO() {
-	glCreateBuffers(1, &VBO);  // Create VBO (Vertex Buffer Object - unsigned ints storing texture, mesh, shader)
-	glNamedBufferStorage(VBO, sizeof(float) * vertexData.size(), vertexData.data(), GL_DYNAMIC_STORAGE_BIT);  // Creates and initialises a buffer object's immutable data store
-
-	glCreateBuffers(1, &EBO);  // Create Element Buffer
-	glNamedBufferStorage(EBO, sizeof(unsigned int) * indices.size(), indices.data(), GL_DYNAMIC_STORAGE_BIT);
-
-	glCreateVertexArrays(1, &VAO);  // Creates VAO (Vertex Array Object - stores VBOs)
-	glVertexArrayVertexBuffer(VAO, 0, VBO, 0, sizeof(float) * 6);  // Binds a buffer to a vertex buffer bind point (stride: 6)
-	glVertexArrayElementBuffer(VAO, EBO);
-	glEnableVertexArrayAttrib(VAO, 0);  // Turns on the array in VAO at specified index
-	glEnableVertexArrayAttrib(VAO, 1);
-	glVertexArrayAttribFormat(VAO, 0, 3, GL_FLOAT, GL_FALSE, 0);  // Specify the organisation of vertex arrays (normalised: GL_FALSE)
-	glVertexArrayAttribFormat(VAO, 1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float));
-	glVertexArrayAttribBinding(VAO, 0, 0);  // assign a vertex attribute to a vertex buffer binding for a vertex array object
-	glVertexArrayAttribBinding(VAO, 1, 0);
+MyScene::~MyScene() {
+	delete m_myShader;
+	delete m_cube;
+	delete m_directionalLight;
 }
 void MyScene::update(float dt) {
 	m_camera->update(dt);
 	render();
 }
 void MyScene::render() {
-	// get uniforms
-	m_model = glm::mat4(1.0f);
-	m_projection = m_camera->getProjectionMatrix();
-	m_view = m_camera->getViewMatrix();
 	// bind shader
 	m_myShader->use();
 	// set uniforms
-	m_myShader->setMat4("Model", m_model);
-	m_myShader->setMat4("Projection", m_projection);
-	m_myShader->setMat4("View", m_view);
-
+	m_myShader->setMat4("Projection", m_camera->getProjectionMatrix());
+	m_myShader->setMat4("View", m_camera->getViewMatrix());
 	m_myShader->setVec3("viewPos", m_camera->getPosition());
-	m_myShader->setVec3("cubeColour", glm::vec3(0.5, 0.0, 0.0));
-	m_myShader->setFloat("shine", 64);
-	m_myShader->setFloat("specStrength", 16);
+	
+	glBindVertexArray(m_cube->getVAO());
+	m_cube->setTransform(m_myShader);
+	glDrawElements(GL_TRIANGLES, m_cube->getIndicesCount(), GL_UNSIGNED_INT, 0);
 
-	if (m_handler->keyHasBeenPressed()) {
-		if (m_handler->isKeyPressed(GLFW_KEY_UP)) {
-			// cubeJump();
-			rot.y++;
-		}
-		if (m_handler->isKeyPressed(GLFW_KEY_RIGHT)) {
-			// x_pos += 0.1f;
-			rot.x++;
-		}
-		if (m_handler->isKeyPressed(GLFW_KEY_LEFT)) {
-			// x_pos -= 0.1f;
-			rot.z++;
-		}
-	}
-	if (jumping) {
-		pos += glm::vec3(0, (25 - jumpFrame) * 0.01f, 0);
-		jumpFrame++;
-		if (jumpFrame == 51)
-			jumping = false;
-	}
-	m_model = glm::translate(m_model, pos);
-	//m_model = glm::rotate(m_model, (float)(glfwGetTime() * 5.0), glm::vec3(0.0, 1.0, 0.0));
-	//m_model = glm::rotate(m_model, 1000.f, rot);
-	m_model = glm::scale(m_model, scale);
-
-	m_myShader->setMat4("Model", m_model);
-	// draw call
-	glBindVertexArray(VAO);
-	glDrawElements(GL_TRIANGLES, vertexData.size(), GL_UNSIGNED_INT, 0);
-}
-void MyScene::cubeJump() {
-	if (jumping) return;
-	jumping = true;
-	jumpFrame = 0;
+	m_cube->translate(glm::vec3(5.0, 0.0, 0.0));
+	m_cube->rotate((float)(glfwGetTime() * 0.5), glm::vec3(1.0, 0.0, 0.0));
+	m_cube->setTransform(m_myShader);
+	glDrawElements(GL_TRIANGLES, m_cube->getIndicesCount(), GL_UNSIGNED_INT, 0);
+	m_cube->resetTransform();
 }
